21a4a16a1cd01a862cd14e080200ec3cb9d30f0c
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2021-04-27 16:28:23 -0400

    Initial Commit

---
+++ TP1_Rebeca_Pacheco_Correa/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TP1_Rebeca_Pacheco_Correa
+//
+//  Created by Rebeca Pacheco on 27/04/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
